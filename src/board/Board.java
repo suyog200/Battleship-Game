37235bc16f6955b5ad609fa27ca39dea9c3b8d84
savio <savio@fedora>
2022-12-15 23:45:36 +0530

    Getters and Settters for square

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,5 +1,12 @@
 package board;
 
 public class Board {
+	public final int BOARD_SIZE = 10;
 
+    private final Square[][] ocean = new Square[BOARD_SIZE][BOARD_SIZE];
+    
+    
+    public Square[][] getOcean() {
+        return ocean;
+    }
 }
