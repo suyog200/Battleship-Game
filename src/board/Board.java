be9e00bf4d29bcc0bfa3857d2fd62c72a656f39a
AmayGawas <amaygawas07@gmail.com>
2022-12-17 13:03:10 +0530

    resolved errors

--- src/board/Board.java
+++ src/board/Board.java
@@ -1,7 +1,38 @@
 package board;
 
 public class Board {
-	//comment
-	//this is my second test comment
-	 public final int BOARD_SIZE = 10;
+
+	public final int BOARD_SIZE = 10;
+
+	private final Square[][] ocean = new Square[BOARD_SIZE][BOARD_SIZE];
+
+	public Square[][] getOcean() {
+		return ocean;
+	}
+
+	public Board() {
+		for (int i = 0; i < BOARD_SIZE; i++) {
+			for (int j = 0; j < BOARD_SIZE; j++) {
+				this.ocean[i][j] = new Square(i, j);
+			}
+		}
+	}
+
+	public void setBoardVisibility(boolean status) {
+		for (Square[] row : ocean) {
+			for (Square col : row) {
+				col.setVisibility(status);
+			}
+		}
+	}
+
+	public boolean isPlacementOk(int[] placement) {
+		int positionX = placement[0];
+		int positionY = placement[1];
+		return 0 <= positionX && positionX < BOARD_SIZE && 0 <= positionY && positionY < BOARD_SIZE && // Is it on
+																										// board?
+				!ocean[positionX][positionY].isShip(); // DO NOT put it on other ships
+	}
+
+
 }
---
+++ src/board/Square.java
@@ -0,0 +1,59 @@
+package board;
+
+public class Square {
+	private boolean isShip, isHit, isHidden, isSunk;
+	private final int x, y;
+
+	public Square(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.isShip = false;
+		this.isHit = false;
+		this.isHidden = false;
+		this.isSunk = false;
+	}
+
+	public boolean isShip() {
+		return isShip;
+	}
+
+	public void setShip(boolean isShip) {
+		this.isShip = isShip;
+	}
+
+	public boolean isHit() {
+		return isHit;
+	}
+
+	public void setHit(boolean isHit) {
+		this.isHit = isHit;
+	}
+
+	public boolean isHidden() {
+		return isHidden;
+	}
+
+	public void setHidden(boolean isHidden) {
+		this.isHidden = isHidden;
+	}
+
+	public void setVisibility(boolean state) {
+		this.isHidden = state;
+	}
+
+	public boolean isSunk() {
+		return isSunk;
+	}
+
+	public void setSunk(boolean isSunk) {
+		this.isSunk = isSunk;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
