a47d435911a992ffe1bb64faf1c81bd6c3354048
AmayGawas <amaygawas07@gmail.com>
2022-12-18 20:07:22 +0530

    Merge branch 'master' of https://github.com/suyog200/Battleship-Game.git

--- src/player/Input.java
+++ src/player/Input.java
@@ -1,5 +1,25 @@
 package player;
 
+
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Input {
+	 public static Position readPosition(Scanner sc, Board board, String message){
+	        try {
+	            System.out.print(message);
+	            String s = sc.nextLine().toLowerCase();
+	            char row = s.charAt(0);
+	            int column = Integer.parseInt(s.substring(1));
+	            Position.isInRange(row, column, board);
+	            return new Position(row, column - 1);
+	        } catch (PositionException | NumberFormatException | StringIndexOutOfBoundsException e){
+	            Display.printError("Errore, valori consentiti tra a1 e " + Position.encode(board.getLength() - 1) + board.getLength());
+	            return readPosition(sc, board, message);
+	        }
+
+	    }
 
 }
