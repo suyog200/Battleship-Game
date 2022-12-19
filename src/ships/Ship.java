bb01f33371b4bb53e46e5c4bb28db5af71765137
Sneharsh <82636385+sneharshkerkar@users.noreply.github.com>
2022-12-19 13:01:34 +0530

    defined class ship's initial variables and constructor

--- src/ships/Ship.java
+++ src/ships/Ship.java
@@ -1,5 +1,18 @@
 package ships;
 
+import board.Square;
+
 public class Ship {
 
+    private List<Square> squares;
+    private boolean isSunk;
+    private ShipType shipType;
+    Display display = new Display();
+
+
+    public Ship(ArrayList<Square> squares, ShipType shipType) {
+        this.squares = squares;
+        this.isSunk = false;
+        this.shipType = shipType;
+    }
 }
