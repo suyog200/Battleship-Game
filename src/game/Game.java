e29e7f655951f5a900efb6544ad03b2e08f8218f
suyog200 <suygchari@gmail.com>
2022-12-21 00:29:28 +0530

    did objects of classes in Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,13 @@
 package game;
 
+import board.Board;
+import utilities.Display;
+import utilities.Input;
+
 public class Game {
+	Display display = new Display();
+	Board board1 = new Board();
+	Board board2 = new Board();
+	Input input = new Input();
 
 }
