3558efaf2ea098aff7606f1394efdcd1434d151f
Sneharsh <82636385+sneharshkerkar@users.noreply.github.com>
2022-12-21 16:59:29 +0530

    Created ShipType class, added ship types & set its length

--- src/ships/ShipType.java
+++ src/ships/ShipType.java
@@ -1,5 +1,25 @@
 package ships;
 
-public class ShipType {
+//different types of ships
+public enum ShipType {
+  Carrier(5),
+  Cruiser(4),
+  Battleship(3),
+  Submarine(3),
+  Destroyer(2)
+  ;
+
+	//to set ship length
+  int length;
+
+  ShipType(int length) {
+      this.length = length;
+  }
+
+  public int getLength() {
+      return length;
+  }
 
 }
+
+
