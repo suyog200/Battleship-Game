27dfc1f54b3d0aa610f2982bf682a286e370c5e6
Sneharsh <82636385+sneharshkerkar@users.noreply.github.com>
2022-12-22 01:11:45 +0530

    added get string & get int methods with functionality

--- src/utilities/Input.java
+++ src/utilities/Input.java
@@ -1,10 +1,36 @@
 package utilities;
 
-public class Input {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-	public String getString(String string) {
-		// TODO Auto-generated method stub
-		return null;
+public class Input {
+      
+	private final Scanner input = new Scanner(System.in);
+    private final Display display = new Display();
+	
+  //get input string
+	public String getString(String message) {
+		Scanner sc = new Scanner(System.in); 
+        display.shout(message);
+
+        String text = "";
+        do {
+            text = sc.nextLine();
+        }
+        while (text.length() < 1);
+        return text;
 	}
+	
+	 //get integer
+    public int getInt(String message) {
+        display.shout(message);
+        try {
+            return input.nextInt();
+        } catch (NumberFormatException | InputMismatchException e) {
+            display.shout("Invalid input, please try again.\n");
+            getInt(message);
+        }
+        return -1;
+    }
 
 }
