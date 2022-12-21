d8e67d59e5d1ee149c30a00a87f3a9537403ede6
AmayGawas <amaygawas07@gmail.com>
2022-12-21 15:15:43 +0530

    made changes to input.java( deleted duplicate)

--- src/player/Player.java
+++ src/player/Player.java
@@ -1,5 +1,32 @@
 package player;
-
-public class Player {
-
+import board.Board;
+import ships.Ship;
+import java.util.Arraylist;
+public class Player extends IPlayer{
+	
+	public Player(String name, Board board, int rank, String symbol) {
+		this.name = name;
+		this.board =board;
+		this.rank= rank;
+		this.symbol= symbol;
+	}
+	public Player(String name)
+	{
+		this.name= name;
+	}
+	public Player(Board board)
+	{
+		this.board= board;
+	}
+	public Player()
+	{
+		
+	}
+	public Boolean IsAlive()
+	{
+		for (Ship ship: this.ships) {
+			if(!ship.isSunk()) return true;
+		}
+		return false;
+	}
 }
