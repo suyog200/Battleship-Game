f7278971fa4149a59447d1d39750f3d715a8359d
savio <savio@fedora>
2022-12-22 12:16:07 +0530

    checkNeighbours() function added logic

--- src/board/BoardFactory.java
+++ src/board/BoardFactory.java
@@ -30,6 +30,7 @@ public class BoardFactory {
         return new Integer[]{new Random().nextInt(player.getBoard().getOcean().length), new Random().nextInt(player.getBoard().getOcean().length)};
     }
     
+    //Manual placement of ship
     public void manualPlacement(Player player, ShipType shipType) {
     	display.shout(String.format("Time to place your %s (size: %s), %s" , shipType, shipType.getLength(), player.name));
         String direction = isInputHorizontalOrVertical();
@@ -56,6 +57,8 @@ public class BoardFactory {
         }
     }
     
+    //check for coordinates, shiplength, direction, shiptype.
+    //return true if it's valid place to put ship
     public Boolean validPlace(IPlayer player, Integer shipSize, Integer[] coordinate, String direction) {
     	Integer[] targetCoordinate;
         try {
@@ -74,10 +77,25 @@ public class BoardFactory {
         return true;
     }
     
+    //Check placement of neighbors ship
     public Boolean checkNeighbours(Integer[] coordinate, String direction, IPlayer player) {
-		return null;
+        if (direction.equals("h")) {
+        	Integer[] topNeighbour = new Integer[]{coordinate[0] - 1, coordinate[1]};
+        	Integer[] bottomNeighbour = new Integer[]{coordinate[0] + 1, coordinate[1]};
+            if (topNeighbour[0] < 0) return player.getBoard().isPlacementOk(bottomNeighbour);
+            else if (bottomNeighbour[0] >= player.getBoard().BOARD_SIZE) return player.getBoard().isPlacementOk(topNeighbour);
+            else return player.getBoard().isPlacementOk(topNeighbour) && player.getBoard().isPlacementOk(bottomNeighbour);
+        }
+        else {
+        	Integer[] leftNeighbour = new Integer[]{coordinate[0], coordinate[1] - 1};
+        	Integer[] rightNeighbour = new Integer[]{coordinate[0], coordinate[1] + 1};
+            if (leftNeighbour[1] < 0) return player.getBoard().isPlacementOk(rightNeighbour);
+            else if (rightNeighbour[1] >= player.getBoard().BOARD_SIZE) return player.getBoard().isPlacementOk(leftNeighbour);
+            else return player.getBoard().isPlacementOk(leftNeighbour) && player.getBoard().isPlacementOk(rightNeighbour);
+        }
     }
     
+    //Setting placement of ship
     public void setPlacement(Integer[] coordinate, IPlayer player, String direction, ShipType shipType) { //Marks chosen Squares as SHIP.
         ArrayList<Square> squares = new ArrayList<>();
         for (int i=0; i<shipType.getLength(); i++) {
@@ -93,6 +111,7 @@ public class BoardFactory {
         player.addShip(ship);
     }
 	
+    //Returns input from Input.java, validate and set h or v if input is true
     public String isInputHorizontalOrVertical() {
         Boolean isOk = false;
         String string;
